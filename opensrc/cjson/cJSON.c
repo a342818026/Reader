16c6fd54153bea067a26abeed32be9d362f68208
npt-1707 <npthanh132@gmail.com>
2026-03-16 00:37:01 +0800

    opensrc/cjson/cJSON.c

--- opensrc/cjson/cJSON.c
+++ opensrc/cjson/cJSON.c
@@ -1783,7 +1783,7 @@ static cJSON *get_object_item(const cJSON * const object, const char * const nam
     current_element = object->child;
     if (case_sensitive)
     {
-        while ((current_element != NULL) && (strcmp(name, current_element->string) != 0))
+        while ((current_element != NULL) && (current_element->string != NULL) && (strcmp(name, current_element->string) != 0))
         {
             current_element = current_element->next;
         }
@@ -1794,6 +1794,9 @@ static cJSON *get_object_item(const cJSON * const object, const char * const nam
         {
             current_element = current_element->next;
         }
+    }
+    if ((current_element == NULL) || (current_element->string == NULL)) {
+        return NULL;
     }
 
     return current_element;
